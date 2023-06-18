440f4806ffd969ca7098c07dd306b6175585feb6
ICT Academy <ict@mainlab14.ictkerala.org>
2023-06-18 16:29:38 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors())  // cor policy activation
 app.use(express.json()); // to render json req from frontend
 app.use(express.urlencoded({extended:true})); // to render form data from frontend
 
-mongoose.connect('mongodb+srv://jumanathasneem653:q7MLE3i7h3HNOBTQ@cluster0.jjhm4pj.mongodb.net/')
+mongoose.connect('mongodb+srv://jumanathasneem653:q7MLE3i7h3HNOBTQ@cluster0.jjhm4pj.mongodb.net/MES')
 .then(()=>{console.log("Mongo connected")})
 .catch((err)=>{console.log(err)})
 
@@ -17,12 +17,18 @@ mongoose.connect('mongodb+srv://jumanathasneem653:q7MLE3i7h3HNOBTQ@cluster0.jjhm
 //R-READ-GET
 //U-Update -PUT
 //D-Delete - DELETE
+const PRODUCT = require("./model/product")
 
-app.post('/addData', (req, res) => {
+app.post('/addData',async (req, res) => {
     try {
 
         let item = req.body
         console.log(item)
+        const saveData = await PRODUCT(item) //PRODUCT is a model. we cross check the form data with the model we created before saving it
+        console.log(saveData)
+
+        await saveData.save()  // through this code we save the incoming data from front end to db 
+        res.send((saveData))
 
 
 
